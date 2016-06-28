b0b5bf13d78e3f45c897b56506be209a55593934
Tejash <tejashdesai241@gmail.com>
2016-06-28 16:00:59 +0530

    added 2 more apis

--- general_apis.py
+++ general_apis.py
@@ -11,3 +11,16 @@ output: a list of files from the path
 def get_files(mypath):
 	only_files = [f for f in listdir(mypath) if isfile(join(mypath,f))]
 	return only_files
+"""
+returns the exception that is stored in the system
+"""
+import sys
+def get_exception_message():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
