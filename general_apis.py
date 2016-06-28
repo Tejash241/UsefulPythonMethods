4405602b9e84a88eb493313718e8549f7661250f
Tejash <tejashdesai241@gmail.com>
2016-06-28 16:26:55 +0530

    added get_immediate_subdirectories

--- general_apis.py
+++ general_apis.py
@@ -10,17 +10,25 @@ output: a list of files from the path
 """
 def get_files(mypath):
 	only_files = [f for f in listdir(mypath) if isfile(join(mypath,f))]
-	return only_files
+	return only_file
+
+"""
+returns the immediate subdirectory list
+"""
+def get_immediate_subdirectories(dir):
+    return [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
+    
 """
 returns the exception that is stored in the system
 """
 import sys
+import linecache
 def get_exception_message():
     exc_type, exc_obj, tb = sys.exc_info()
-    f = tb.tb_frame
+    f = tb.
+    tb_frame
     lineno = tb.tb_lineno
     filename = f.f_code.co_filename
     linecache.checkcache(filename)
     line = linecache.getline(filename, lineno, f.f_globals)
     return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
-
