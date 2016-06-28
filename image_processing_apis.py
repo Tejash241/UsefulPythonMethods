7489b427109f1e7cbfa8f61eedf7082de99ebd8c
Tejash <tejashdesai241@gmail.com>
2016-06-28 12:08:32 +0530

    added url_imread() and rgb2gray()

--- image_processing_apis.py
+++ image_processing_apis.py
@@ -1,6 +1,3 @@
-import cv2
-
-
 def are_images_same(image, reference_image):
 	return image.shape == reference_image.shape and not(np.bitwise_xor(image,reference_image).any())
 
@@ -18,3 +15,20 @@ def sort_contours(contours, method="left-to-right"):
 	(cnts, bounding_boxes) = zip(*sorted(zip(cnts, bounding_boxes),
 		key=lambda b:b[1][i], reverse=reverse))
 	return (cnts)
+
+"""
+Returns an image from a url. Can also be done via urllib.urlretrieve()
+"""
+import urllib
+def url_imread(url):
+    req = urllib.urlopen(url)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    img = cv2.imdecode(arr, -1) # 'load it as it is'
+    return img
+
+"""
+converts rgb image into gray scale. Can also use cv2.cvtToGray()
+"""
+import numpy as np
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
